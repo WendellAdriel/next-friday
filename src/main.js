111f9a35ce43e2d6ea864f4a229e38a3e0c3cbe9
Wendell Adriel <wendelladriel.ti@gmail.com>
2018-01-26 23:51:20 +0000

    finished first version

--- src/main.js
+++ src/main.js
@@ -1,10 +1,69 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import moment from 'moment'
+import '../node_modules/bulma/css/bulma.css'
 
-class HelloMessage extends React.Component {
-  render () {
-    return <div>Hello {this.props.name}</div>
+const getDifference = (targetDay, targetHour) => {
+  const targetDate = moment()
+
+  if (targetDate.isoWeekday() <= targetDay && targetDate.hour() < targetHour) {
+    return moment.duration(
+      targetDate
+        .isoWeekday(targetDay)
+        .set({
+          hour: targetHour,
+          minute: 0,
+          second: 0,
+          millisecond: 0
+        })
+        .diff(moment())
+    )
   }
+  return moment.duration(
+    targetDate
+      .add(1, 'weeks')
+      .isoWeekday(targetDay)
+      .set({
+        hour: targetHour,
+        minute: 0,
+        second: 0,
+        millisecond: 0
+      })
+      .diff(moment())
+  )
 }
 
-ReactDOM.render(<HelloMessage name="Wendell" />, document.getElementById('app'))
+const App = ({ targetDay, targetHour }) => {
+  const diff = getDifference(targetDay, targetHour)
+  return (
+    <section className="hero is-dark is-fullheight">
+      <div className="hero-body">
+        {diff && (
+          <div className="container has-text-centered">
+            <h1 className="title is-size-1">{`${diff.days()} days, ${diff.hours()} hours, ${diff.minutes()} minutes and ${diff.seconds()} seconds`}</h1>
+            <h2 className="subtitle is-size-3">
+              Until{' '}
+              {moment()
+                .isoWeekday(targetDay)
+                .format('dddd')}{' '}
+              at{' '}
+              {moment()
+                .set({
+                  hour: targetHour,
+                  minute: 0,
+                  second: 0,
+                  millisecond: 0
+                })
+                .format('HH:mm:ss')}
+            </h2>
+          </div>
+        )}
+      </div>
+    </section>
+  )
+}
+
+const renderApp = () =>
+  render(<App targetDay={5} targetHour={17} />, document.getElementById('app'))
+
+setInterval(renderApp, 1000)
